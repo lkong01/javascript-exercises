e7f3814c6e885447e6245e3e4e08da1af497be4d
k <lingweikong10@gmail.com>
2022-08-02 18:12:35 -0500

    solve first four questions, no need 5 and 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    for (const num of nums){
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] === num){
+                arr.splice(i, 1)
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
